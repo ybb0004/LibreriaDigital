baaf74d0428db892992f7629a4c87761b128f22c
ybb0004 <ybb0004@alu.medac.es>
2025-01-13 13:07:40 +0100

    COMMIT 1
    Creacion de interfaz principal y primera pestaña

---
+++ LibreriaDigital/src/main/java/com/youssefbenavente/libreriadigital/LibreriaDigital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.youssefbenavente.libreriadigital;
+
+/**
+ *
+ * @author Youssef Benavente
+ */
+public class LibreriaDigital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
